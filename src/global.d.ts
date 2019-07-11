24387c90992723a41aa99afc3663e508a07036c5
Kyle Andrews <kylea@page.works>
2019-07-11 19:30:55 -0400

    Reset back to a full express app

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,3 +1,8 @@
+interface IServer{
+    listen: (port:number, response:Function)=>void;
+    get: Function;
+}
+
 interface IExpress{
     get: (req:any, res:any)=>void;
     listen: (port:number)=>void;
