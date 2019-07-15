b2cb2de6a1cbe3db43ed90164a7729c82c6e33b8
Kyle Andrews <kylea@page.works>
2019-07-15 10:26:41 -0400

    Updates Server class methods

--- Server.js
+++ Server.js
@@ -6,15 +6,20 @@ class Server {
         this._app = express();
         this._app.listen(8181);
         this._app.use(express.static('public'));
-        this._app.get('/', (req, res) => {
-            res.sendFile(`${__dirname}/public/index.html`);
-        });
-        this._app.post('/upload', upload.single('translation'), (req, res) => {
-            if (!req.file) {
-                return res.status(400).send('No files were uploaded.');
-            }
-            return res.status(200).send('Upload successful.');
-        });
+        this.router();
+    }
+    router() {
+        this._app.get('/', this.homepage);
+        this._app.post('/upload', upload.single('translation'), this.upload);
+    }
+    homepage(req, res) {
+        return res.status(200).sendFile(`${__dirname}/public/index.html`);
+    }
+    upload(req, res) {
+        if (!req.file) {
+            return res.status(400).send('No files were uploaded.');
+        }
+        return res.status(200).send('Upload successful.');
     }
 }
 new Server();
--- src/Server.ts
+++ src/Server.ts
@@ -10,23 +10,29 @@ class Server
     {
         this._app = express();
         this._app.listen(8181);
-
         this._app.use(express.static('public'));
-        
-        this._app.get('/', (req:IExpressRequest, res:IExpressResponse) => {
-            // console.log(req);
-            // console.log(res);
-            res.sendFile(`${ __dirname }/public/index.html`);
-        });
-        
-        this._app.post('/upload', upload.single('translation'), (req:IExpressRequest, res:IExpressResponse) => {
-            if (!req.file)
-            {
-                return res.status(400).send('No files were uploaded.');
-            }
+        this.router();
+    }
 
-            return res.status(200).send('Upload successful.');
-        });
+    private router() : void
+    {
+        this._app.get('/', this.homepage);
+        this._app.post('/upload', upload.single('translation'), this.upload);
+    }
+
+    private homepage(req:IExpressRequest, res:IExpressResponse) : IExpressResponse
+    {
+        return res.status(200).sendFile(`${ __dirname }/public/index.html`);
+    }
+
+    private upload(req:IExpressRequest, res:IExpressResponse) : IExpressResponse
+    {
+        if (!req.file)
+        {
+            return res.status(400).send('No files were uploaded.');
+        }
+
+        return res.status(200).send('Upload successful.');
     }
 }
 
