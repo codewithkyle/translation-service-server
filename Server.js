49293a7ff411de0748f07efcad8c7a4af2ad2ec9
Kyle Andrews <kylea@page.works>
2019-07-15 11:06:49 -0400

    Updates upload response logic for better error reporting

--- Server.js
+++ Server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const upload = multer({ dest: 'uploads/' });
+const fs = require('fs');
 class Server {
     constructor() {
         this._app = express();
@@ -9,8 +10,8 @@ class Server {
         this.router();
     }
     router() {
-        this._app.get('/', this.homepage);
-        this._app.post('/upload', upload.single('translation'), this.upload);
+        this._app.get('/', this.homepage.bind(this));
+        this._app.post('/upload', upload.single('translation'), this.upload.bind(this));
     }
     homepage(req, res) {
         return res.status(200).sendFile(`${__dirname}/public/index.html`);
@@ -19,7 +20,42 @@ class Server {
         if (!req.file) {
             return res.status(400).send('No files were uploaded.');
         }
-        return res.status(200).send('Upload successful.');
+        new Promise((resolve, reject) => {
+            (async () => {
+                try {
+                    const json = await this.getJsonFromFile(req.file);
+                    await resolve();
+                }
+                catch (err) {
+                    reject(err);
+                }
+            })();
+        })
+            .then(() => {
+            return res.status(200).send('Upload successful.');
+        })
+            .catch(error => {
+            return res.status(500).send(error);
+        });
+    }
+    getJsonFromFile(file) {
+        return new Promise((resolve, reject) => {
+            switch (file.mimetype) {
+                case 'text/csv':
+                    break;
+                case 'application/json':
+                    fs.readFile(file.path, (err, file) => {
+                        if (err) {
+                            reject('Failed to open JSON file.');
+                        }
+                        resolve(JSON.parse(file));
+                    });
+                    break;
+                default:
+                    reject('Invalid file type. Upload a CSV or JSON file.');
+                    break;
+            }
+        });
     }
 }
 new Server();
--- src/Server.ts
+++ src/Server.ts
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer  = require('multer');
 const upload = multer({ dest: 'uploads/' });
+const fs = require('fs');
 
 class Server
 {
@@ -16,8 +17,8 @@ class Server
 
     private router() : void
     {
-        this._app.get('/', this.homepage);
-        this._app.post('/upload', upload.single('translation'), this.upload);
+        this._app.get('/', this.homepage.bind(this));
+        this._app.post('/upload', upload.single('translation'), this.upload.bind(this));
     }
 
     private homepage(req:IExpressRequest, res:IExpressResponse) : IExpressResponse
@@ -32,7 +33,50 @@ class Server
             return res.status(400).send('No files were uploaded.');
         }
 
-        return res.status(200).send('Upload successful.');
+        new Promise((resolve, reject)=>{
+            (async ()=>{
+                try{
+                    const json = await this.getJsonFromFile(req.file);
+                    await resolve();
+                }
+                catch(err)
+                {
+                    reject(err);
+                }
+            })();
+        })
+        .then(()=>{
+            return res.status(200).send('Upload successful.');
+        })
+        .catch(error => {
+            return res.status(500).send(error);
+        });
+    }
+
+    private getJsonFromFile(file:IFile) : Promise<any>
+    {
+        return new Promise((resolve, reject)=>{
+            switch(file.mimetype)
+            {
+                case 'text/csv':
+                    /** TODO: Convert CSV to JSON */
+                    break;
+                case 'application/json':
+                    // @ts-ignore
+                    fs.readFile(file.path, (err, file)=>{
+                        if(err)
+                        {
+                            reject('Failed to open JSON file.');
+                        }
+
+                        resolve(JSON.parse(file));
+                    });
+                    break;
+                default:
+                    reject('Invalid file type. Upload a CSV or JSON file.');
+                    break;
+            }
+        });
     }
 }
 
