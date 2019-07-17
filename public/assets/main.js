5d5c9cf580adb1490fd9e9f0d99cd70ccc0f1ef3
Kyle Andrews <kylea@page.works>
2019-07-17 14:09:52 -0400

    Adds drag and drop upload logic

--- public/assets/main.js
+++ public/assets/main.js
@@ -1,32 +1,4 @@
-var file = null;
 var translations = null;
-var fileInput = document.body.querySelector('input#fileInput');
-fileInput.addEventListener('change', function (e) {
-    var target = e.currentTarget;
-    uploadFile(target.files[0]);
-});
-function uploadFile(file) {
-    var data = new FormData();
-    data.append('translation', file);
-    fetch(window.location.origin + "/upload", {
-        headers: new Headers({
-            'X-Requested-With': 'XMLHttpRequest',
-            'Accept': 'application/json',
-        }),
-        credentials: 'include',
-        method: 'POST',
-        body: data
-    })
-        .then(function (request) { return request.json(); })
-        .then(function (response) {
-        if (response) {
-            translations = response;
-        }
-    })
-        .catch(function (error) {
-        console.error(error);
-    });
-}
 var convertButton = document.body.querySelector('button');
 convertButton.addEventListener('click', function (e) {
     if (!translations) {
@@ -53,4 +25,66 @@ convertButton.addEventListener('click', function (e) {
         console.error(error);
     });
 });
+var UploadPrompt = /** @class */ (function () {
+    function UploadPrompt() {
+        this.handleFileUpload = this.change.bind(this);
+        this.handleDragEnter = this.dragEnter.bind(this);
+        this.handleDragLeave = this.dragLeave.bind(this);
+        this.handleDrop = this.drop.bind(this);
+        this.view = document.body.querySelector('upload-prompt');
+        this._fileInput = this.view.querySelector('input#fileInput');
+        this._fileInputLabel = this.view.querySelector('label');
+        this.init();
+    }
+    UploadPrompt.prototype.init = function () {
+        this._fileInput.addEventListener('change', this.handleFileUpload);
+        this._fileInputLabel.addEventListener('dragenter', this.handleDragEnter);
+        this._fileInputLabel.addEventListener('dragleave', this.handleDragLeave);
+        this._fileInputLabel.addEventListener('dragover', function (e) { e.preventDefault(); });
+        this._fileInputLabel.addEventListener('drop', this.handleDrop);
+    };
+    UploadPrompt.prototype.drop = function (e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        if (e.dataTransfer.files.length) {
+            this.uploadFile(e.dataTransfer.files[0]);
+        }
+    };
+    UploadPrompt.prototype.dragEnter = function () {
+        this._fileInputLabel.classList.add('is-prompting');
+    };
+    UploadPrompt.prototype.dragLeave = function () {
+        this._fileInputLabel.classList.remove('is-prompting');
+    };
+    UploadPrompt.prototype.change = function () {
+        if (!this._fileInput.files.length) {
+            return;
+        }
+        this.uploadFile(this._fileInput.files[0]);
+    };
+    UploadPrompt.prototype.uploadFile = function (file) {
+        var data = new FormData();
+        data.append('translation', file);
+        fetch(window.location.origin + "/upload", {
+            headers: new Headers({
+                'X-Requested-With': 'XMLHttpRequest',
+                'Accept': 'application/json',
+            }),
+            credentials: 'include',
+            method: 'POST',
+            body: data
+        })
+            .then(function (request) { return request.json(); })
+            .then(function (response) {
+            if (response) {
+                translations = response;
+            }
+        })
+            .catch(function (error) {
+            console.error(error);
+        });
+    };
+    return UploadPrompt;
+}());
+new UploadPrompt();
 //# sourceMappingURL=main.js.map
--- public/main.ts
+++ public/main.ts
@@ -1,37 +1,5 @@
-let file = null;
-let translations:any = null;
 
-const fileInput:HTMLInputElement = document.body.querySelector('input#fileInput');
-fileInput.addEventListener('change', (e:Event)=>{
-    const target = <HTMLInputElement>e.currentTarget;
-    uploadFile(target.files[0]);
-});
-
-function uploadFile(file:File)
-{
-    const data = new FormData();
-    data.append('translation', file);
-
-    fetch(`${ window.location.origin }/upload`, {
-        headers: new Headers({
-            'X-Requested-With': 'XMLHttpRequest',
-            'Accept': 'application/json',
-        }),
-        credentials: 'include',
-        method: 'POST',
-        body: data
-    })
-    .then(request => request.json())
-    .then(response => {
-        if(response)
-        {
-            translations = response;
-        }
-    })
-    .catch(error => {
-        console.error(error);
-    });
-}
+let translations:any = null; 
 
 const convertButton:HTMLButtonElement = document.body.querySelector('button');
 convertButton.addEventListener('click', (e:Event)=>{
@@ -61,3 +29,93 @@ convertButton.addEventListener('click', (e:Event)=>{
         console.error(error);
     });
 });
+
+class UploadPrompt{
+
+    public view : HTMLElement;
+    private _fileInput : HTMLInputElement;
+    private _fileInputLabel : HTMLElement;
+
+    constructor()
+    {
+        this.view = document.body.querySelector('upload-prompt');
+        this._fileInput = this.view.querySelector('input#fileInput');
+        this._fileInputLabel = this.view.querySelector('label');
+
+        this.init();
+    }
+
+    private handleFileUpload:EventListener = this.change.bind(this);
+    private handleDragEnter:EventListener = this.dragEnter.bind(this);
+    private handleDragLeave:EventListener = this.dragLeave.bind(this);
+    private handleDrop:EventListener = this.drop.bind(this);
+
+    private init() : void
+    {
+        this._fileInput.addEventListener('change', this.handleFileUpload);
+
+        this._fileInputLabel.addEventListener('dragenter', this.handleDragEnter);
+        this._fileInputLabel.addEventListener('dragleave', this.handleDragLeave);
+        this._fileInputLabel.addEventListener('dragover', (e:Event)=>{ e.preventDefault(); });
+        this._fileInputLabel.addEventListener('drop', this.handleDrop);
+    }
+
+    private drop(e:DragEvent) : void
+    {
+        e.preventDefault();
+        e.stopImmediatePropagation();        
+
+        if(e.dataTransfer.files.length)
+        {
+            this.uploadFile(e.dataTransfer.files[0]);
+        }
+    }
+
+    private dragEnter() : void
+    {
+        this._fileInputLabel.classList.add('is-prompting');
+    }
+
+    private dragLeave() : void
+    {
+        this._fileInputLabel.classList.remove('is-prompting');
+    }
+
+    private change() : void
+    {
+        if(!this._fileInput.files.length)
+        {
+            return;
+        }
+
+        this.uploadFile(this._fileInput.files[0]);
+    }
+
+    private uploadFile(file:File) : void
+    {
+        const data = new FormData();
+        data.append('translation', file);
+
+        fetch(`${ window.location.origin }/upload`, {
+            headers: new Headers({
+                'X-Requested-With': 'XMLHttpRequest',
+                'Accept': 'application/json',
+            }),
+            credentials: 'include',
+            method: 'POST',
+            body: data
+        })
+        .then(request => request.json())
+        .then(response => {
+            if(response)
+            {
+                translations = response;
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }
+}
+
+new UploadPrompt();
